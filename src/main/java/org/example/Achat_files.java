87f579caa22e80f2d7446edb19151d0a77457a62
Your Name <you@example.com>
2023-03-21 20:52:33 +0000

    first ver

---
+++ src/main/java/org/example/Achat_files.java
@@ -0,0 +1,26 @@
+package org.example;
+
+import org.apache.spark.SparkConf;
+
+public class Achat_files {
+
+    public static void main(String[] args) {
+
+        SparkConf conf = new SparkConf().setMaster("local[*]").setAppName("Spark streaming version");
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+}
